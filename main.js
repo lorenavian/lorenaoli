062938f48c4406121f67f5be3d8ad87e646a38ce
lorenavian <160252089+lorenavian@users.noreply.github.com>
2024-06-17 11:58:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+]
+[
+        const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+        
+            
+];
+
